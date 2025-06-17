31984caf724a3dc6f641f09862a8305d8187bcd7
sriharsha <sriharsha.a@gvkss.com>
2025-06-17 15:48:36 +0530

    Clean history: remove secrets and use environment variables

---
+++ call/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('make-call/', views.make_call, name='make_call'),
+    path('answer/', views.answer_call, name='answer_call'),
+    path('save-recording/', views.save_recording, name='save_recording'),
+]
